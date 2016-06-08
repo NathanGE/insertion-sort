0f32487358350a97e946701d902af6e6f602d411
Nathan <nathangelliott@gmail.com>
2016-06-08 20:04:22 +0000

    Fixed typo in insert function

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,6 +1,5 @@
 function insert(array, rightIndex, value){
-  var i;
-  for(i = rightIndex; i >= 0 && value < array[i]; i--){
+  for(var i = rightIndex; i >= 0 && value < array[i]; i--){
     array[i + 1] = array[i]
   }
   array[i + 1] = value;
@@ -16,3 +15,7 @@ function insertionSort(array){
 var array = [3, 5, 7, 11, 13, 2, 9, 6];
 // [2, 3, 5, 6, 7, 9, 11, 13]
 console.log(insertionSort(array));
+
+var array2 = [4, -5, 17, 3]
+// [-5, 3, 4, 17]
+console.log(insertionSort(array2));
